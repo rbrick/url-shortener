818627da788d6ff6cd0ab614d7cfca09dda0d39c
rbrick <rbrickproductions123@gmail.com>
2016-05-15 01:55:17 -0500

    Add redirect code, and a basic database lookup

--- main.go
+++ main.go
@@ -1,30 +1,48 @@
 package main
 
 import (
-	"time"
 	"net/http"
 	"github.com/garyburd/redigo/redis"
 	"github.com/gorilla/mux"
+	"errors"
+	"log"
+	"strconv"
 )
 
 var redisConn redis.Conn
 
 // A short url, contains the time it was created,
 type ShortUrl struct {
-	Id string `json:"id"`
-	LongUrl string `json:"longUrl"`
+	Id        string `json:"id"`
+	LongUrl   string `json:"longUrl"`
 	Timestamp int64 `json:"timestamp"`
-	Clicks int `json:"clicks"`
+	Clicks    int `json:"clicks"`
 }
 
-// Creates a new short url
-func create(id, longUrl string) ShortUrl {
-	return ShortUrl{id, longUrl, time.Now().Unix(), 0}
+// performs a lookup on the database for a short url
+func lookup(id string) (*ShortUrl, error) {
+	val, err := redis.ByteSlices(redisConn.Do("HMGET", id, "Id", "LongUrl", "Timestamp", "Clicks"))
+	if err != nil {
+		return nil, errors.New("Invalid ID!")
+	}
+	longUrl := string(val[1])
+	timestamp, _ := strconv.Atoi(string(val[2]))
+	clicks, _ := strconv.Atoi(string(val[3]))
+	return &ShortUrl{
+		id,
+		longUrl,
+		int64(timestamp),
+		clicks,
+	}, nil
 }
 
-func lookup(id string) {
+func init() {
+	if r, err := redis.Dial("tcp", "localhost:6379"); err != nil {
+		log.Fatal(err)
+	} else {
+		redisConn = r
+	}
 }
-
 
 func main() {
 	router := mux.NewRouter()
@@ -39,5 +57,10 @@ func main() {
 // Handles redirecting to a long url
 func redirect(w http.ResponseWriter, r *http.Request) {
 	v := mux.Vars(r)
-	w.Write([]byte(v["id"]))
+	s, err := lookup(v["id"])
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+	http.Redirect(w, r, s.LongUrl, http.StatusMovedPermanently)
 }
